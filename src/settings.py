3d568048f43819484cf2346066fd98afb8c63ffb
Tim Andrews <tim.andrews@imagineifinc.com>
2017-06-06 21:22:32 -0400

    Static Files

--- src/settings.py
+++ src/settings.py
@@ -119,3 +119,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, "src/../static_root")
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+]
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, "src/../media_root")
+
