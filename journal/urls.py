6a45460f190e502b8e45b768c58c76d3023b549b
Tim Andrews <tim.andrews@imagineifinc.com>
2017-06-18 01:14:26 -0400

    Basic Journal - 2 - Views

--- journal/urls.py
+++ journal/urls.py
@@ -1,9 +1,22 @@
 from django.conf.urls import url
 
-from journal.views import home, journal, page1
+from journal.views import (
+    home,
+    journal_home,
+    journal_create,
+    journal_delete,
+    journal_detail,
+    journal_list,
+    journal_update,
+    page1
+)
 
 urlpatterns = [
     url(r'^$', home, name='home'),
-    url(r'^journal/$', journal, name='journal'),
+    url(r'^journal/$', journal_home, name='journal'), # List
+    url(r'^journal/create/$', journal_create),
+    url(r'^journal/delete/$', journal_delete),
+    url(r'^journal/detail/$', journal_detail),
+    url(r'^journal/update/$', journal_update),
     url(r'^page1/$', page1, name='page1'),
 ]
--- journal/views.py
+++ journal/views.py
@@ -7,11 +7,26 @@ def home(request):
     }
     return render(request, "home.html", context)
 
-def journal(request):
+def journal_home(request):
     context = {
 
     }
     return render(request, "journal.html", context)
+
+def journal_create(request):
+    return HttpResponse("<h1>Create</h1>")
+
+def journal_detail(request):
+    return HttpResponse("<h1>Detail</h1>")
+
+def journal_list(request):
+    return HttpResponse("<h1>List</h1>")
+
+def journal_update(request):
+    return HttpResponse("<h1>Update</h1>")
+
+def journal_delete(request):
+    return HttpResponse("<h1>Delete</h1>")
 
 def page1(request):
     context = {
