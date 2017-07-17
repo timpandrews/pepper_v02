0c72c2faecc23bb21dcd556b24ea2d7ef67248b6
Tim Andrews <tim.andrews@imagineifinc.com>
2017-07-16 21:42:52 -0400

    clean up & permissions for delete

--- comments/views.py
+++ comments/views.py
@@ -1,12 +1,23 @@
 from django.contrib import messages
-from django.shortcuts import render, get_object_or_404, HttpResponseRedirect
+from django.http import HttpResponseRedirect, Http404, HttpResponse
+from django.shortcuts import render, get_object_or_404
 
 from comments.models import Comment
 
 
 def comment_delete(request, id):
-    comment = get_object_or_404(Comment, id=id)
+    try:
+        comment = Comment.objects.get(id=id)
+    except:
+        raise Http404
+
     journal_URL = comment.content_object.get_absolute_url()
+
+    # check user is owner of comment
+    if comment.user != request.user
+        response = HttpResponse("You do not have permissions to delete this comment")
+        response.status_code = 403
+        return response
 
     # if delete confirmed the delete & return to journal detail page
     if request.method == "POST":
