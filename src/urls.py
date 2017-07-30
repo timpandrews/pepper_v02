baf133000f1bef522bbba11ac4e2d8daff46e3fc
Tim Andrews <tim.andrews@imagineifinc.com>
2017-07-30 19:36:30 -0400

    Change is_staff requirement to login_required
    Manually add urls into src/urls.py & change is_staff decorator to
    login_requred

--- src/urls.py
+++ src/urls.py
@@ -17,16 +17,26 @@ from django.conf import settings
 from django.conf.urls import include, url
 from django.conf.urls.static import static
 from django.contrib import admin
+from django.contrib.auth.decorators import login_required
+from django.views.decorators.cache import never_cache
+
 from journal.views import home
+
+from ckeditor_uploader import views
 
 urlpatterns = [
     url(r'^$', home, name='home'),
     url(r'^admin/', admin.site.urls),
     url(r'^accounts/', include('allauth.urls')),
-    url(r'^ckeditor/', include('ckeditor_uploader.urls')),
     url(r'^', include("journal.urls", namespace="journal")),
     url(r'^', include("comments.urls", namespace="comments")),
 ]
+
+# urlpatterns for ckeditor
+urlpatterns += [
+        url(r'^upload/', login_required(views.upload), name='ckeditor_upload'),
+        url(r'^browse/', never_cache(login_required(views.browse)), name='ckeditor_browse'),
+    ]
 
 if settings.DEBUG:
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
