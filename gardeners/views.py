aa2a10a6d0017aefa8b48c333f8637665bac0c91
Tim Andrews <tim.andrews@imagineifinc.com>
2017-08-05 00:48:21 -0400

    Extend Auth model with Profile Model + Add Profile to Admin

--- gardeners/views.py
+++ gardeners/views.py
@@ -1,9 +1,12 @@
+from django.contrib.auth.models import User
 from django.shortcuts import render
 
 # Create your views here.
 
 def gardeners_home(request):
+    gardeners_list = User.objects.all()
     context = {
+        "gardeners_list": gardeners_list,
         "title": "Gardeners Home",
     }
-    return render(request, "page1.html", context)
+    return render(request, "gardeners.html", context)
