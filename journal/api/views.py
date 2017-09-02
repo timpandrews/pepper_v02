1afc3182a619d2bc5875dbd87ac725de98201ef0
Tim Andrews <tim.andrews@imagineifinc.com>
2017-09-02 19:10:38 -0400

    Permissions & List Filtering/Ordering

--- journal/api/views.py
+++ journal/api/views.py
@@ -1,12 +1,26 @@
+from django.db.models import Q
+
+from rest_framework.filters import (
+    SearchFilter,
+    OrderingFilter,
+)
 from rest_framework.generics import (
     CreateAPIView,
     DestroyAPIView,
     ListAPIView,
     RetrieveAPIView,
+    RetrieveUpdateAPIView,
     UpdateAPIView,
+)
+from rest_framework.permissions import (
+    AllowAny,
+    IsAdminUser,
+    IsAuthenticated,
+    IsAuthenticatedOrReadOnly,
 )
 
 from journal.models import Journal
+from journal.api.permissions import IsOwnerOrReadOnly
 from journal.api.serializers import (
     JournalCreateUpdateSerializer,
     JournalDetailSerializer,
@@ -17,6 +31,10 @@ from journal.api.serializers import (
 class JournalCreateAPIView(CreateAPIView):
     queryset = Journal.objects.all()
     serializer_class = JournalCreateUpdateSerializer
+    permission_classes = [IsAuthenticated]
+
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
 
 
 class JournalDeleteAPIView_by_id(DestroyAPIView):
@@ -42,16 +60,34 @@ class JournalDetailAPIView_by_slug(RetrieveAPIView):
 
 
 class JournalListAPIView(ListAPIView):
-    queryset = Journal.objects.all()
-    serializer_class = JournalListSerializer
+    serializer_class = JournalDetailSerializer
+    filter_backends = [SearchFilter, OrderingFilter]
+    search_fields = [
+        'title',
+        'content',
+        'user__first_name',
+        'user__last_name',
+    ]
+
+    def get_queryset(self, *args, **kwargs):
+        qs_list = Journal.objects.all()
+        query = self.request.GET.get("q")
+        if query:
+            qs_list = qs_list.filter(
+                Q(title__icontains=query) |
+                Q(content__icontains=query) |
+                Q(user__first_name__icontains=query)|
+                Q(user__last_name__icontains=query)
+            ).distinct()
+        return qs_list
 
 
-class JournalUpdateAPIView_by_id(UpdateAPIView):
+class JournalUpdateAPIView_by_id(RetrieveUpdateAPIView):
     queryset = Journal.objects.all()
     serializer_class = JournalCreateUpdateSerializer
 
 
-class JournalUpdateAPIView_by_slug(UpdateAPIView):
+class JournalUpdateAPIView_by_slug(RetrieveUpdateAPIView):
     queryset = Journal.objects.all()
     serializer_class = JournalCreateUpdateSerializer
     lookup_field = 'slug'
